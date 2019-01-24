5eab24caf1c166e2872d5be9a1028cb327291fa7
mza1202 <manuel.antonio@liberty.co.za>
2019-01-24 12:38:38 +0200

    working on variables

--- udemy/java/HelloWorld/src/Hello.java
+++ udemy/java/HelloWorld/src/Hello.java
@@ -6,5 +6,16 @@ public class Hello {
 
         System.out.println("Hello, Manuel!");
 
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myFourthNumber = 1000 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(myFourthNumber);
+
     }
 }
