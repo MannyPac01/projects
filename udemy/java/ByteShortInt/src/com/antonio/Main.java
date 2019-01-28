c87e897cf4b1b09f8d59209fdc02a4a8c0c6663d
mza1202 <manuel.antonio@liberty.co.za>
2019-01-28 14:02:29 +0200

    Java variables

---
+++ udemy/java/ByteShortInt/src/com/antonio/Main.java
@@ -0,0 +1,22 @@
+package com.antonio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // whole numbers
+        // VVVVVVVVVVVVV
+        // int has a width of 32
+        int myMinValue=-2_147_483_648;
+        int myMaxValue=2_147_483_647;
+
+        // byte has a width of 8
+        byte myByteValue = -128;
+
+        // short has a width of 16
+        short myShortValue = 32767;
+
+        // long has a width of 64
+        long myLongValue = 100L;
+    }
+}
